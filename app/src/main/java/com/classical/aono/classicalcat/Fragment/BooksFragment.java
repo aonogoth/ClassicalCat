3ad56f5886a83d54aa08cc51bda793c70294b520
aono <aonogoth@gmail.com>
2017-09-26 21:14:31 +0800

    AAA

---
+++ app/src/main/java/com/classical/aono/classicalcat/Fragment/BooksFragment.java
@@ -0,0 +1,19 @@
+package com.classical.aono.classicalcat.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+/**
+ * Created by gotha on 2017/9/26.
+ */
+
+public class BooksFragment extends Fragment{
+
+
+}
---
+++ app/src/main/java/com/classical/aono/classicalcat/Fragment/CategoryZtmzFragment.java
@@ -0,0 +1,10 @@
+package com.classical.aono.classicalcat.Fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by gotha on 2017/9/26.
+ */
+
+public class CategoryZtmzFragment extends Fragment {
+}
