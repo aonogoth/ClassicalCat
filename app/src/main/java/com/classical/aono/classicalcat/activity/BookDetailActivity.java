b3051ed131910b78b43bc279e6df7b09eb697d93
aonogoth <aonogoth@gmail.com>
2017-09-30 17:16:16 +0800

    wang

--- app/src/main/java/com/classical/aono/classicalcat/activity/BookDetailActivity.java
+++ app/src/main/java/com/classical/aono/classicalcat/activity/BookDetailActivity.java
@@ -1,5 +1,6 @@
 package com.classical.aono.classicalcat.activity;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -16,6 +17,7 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.classical.aono.classicalcat.R;
 import com.classical.aono.classicalcat.common.ThreadPool;
 import com.classical.aono.classicalcat.domain.Book;
@@ -29,15 +31,26 @@ import com.google.android.agera.Repositories;
 import com.google.android.agera.Repository;
 import com.google.android.agera.Result;
 import com.google.android.agera.Updatable;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by admin on 2017/9/27.
  */
 
-public class BookDetailActivity extends AppCompatActivity implements Updatable {
+public class BookDetailActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     //private Book mBook;
     private String workId = "";
@@ -46,6 +59,7 @@ public class BookDetailActivity extends AppCompatActivity implements Updatable {
     private WorkDetailSupplier worksSupplier;
     private Repository<Result<Work>> worksRepository;
     private SearchObservable searchObservable;
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,50 +77,51 @@ public class BookDetailActivity extends AppCompatActivity implements Updatable {
 
         workId = getIntent().getStringExtra("workid");
         //mBook = (Book) getIntent().getSerializableExtra("book");
-        setUpRepository();
-        mBook = worksRepository.get().get();
+        //setUpRepository();
+        //mBook = worksRepository.get().get();
+        getRequest();
 
-        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        collapsingToolbar.setTitle(mBook.getName());
-        //collapsingToolbar.setExpandedTitleColor(Color.BLUE);
-
-        ImageView ivImage = (ImageView) findViewById(R.id.ivImage);
-        Glide.with(ivImage.getContext())
-                .load("http://118.178.95.56:8086/UploadFiles/"+mBook.getImageUrlDetail())
-                .fitCenter()
-                .into(ivImage);
-
-        mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        setupViewPager(mViewPager);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
-        tabLayout.addTab(tabLayout.newTab().setText("速览"));
-        tabLayout.addTab(tabLayout.newTab().setText("简介"));
-        tabLayout.addTab(tabLayout.newTab().setText("花絮"));
-        tabLayout.setupWithViewPager(mViewPager);
+//        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+//        collapsingToolbar.setTitle(mBook.getName());
+//        //collapsingToolbar.setExpandedTitleColor(Color.BLUE);
+//
+//        ImageView ivImage = (ImageView) findViewById(R.id.ivImage);
+//        Glide.with(ivImage.getContext())
+//                .load("http://118.178.95.56:8086/UploadFiles/"+mBook.getImageUrlDetail())
+//                .fitCenter()
+//                .into(ivImage);
+//
+//        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+//        setupViewPager(mViewPager);
+//
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+//        tabLayout.addTab(tabLayout.newTab().setText("速览"));
+//        tabLayout.addTab(tabLayout.newTab().setText("简介"));
+//        tabLayout.addTab(tabLayout.newTab().setText("花絮"));
+//        tabLayout.setupWithViewPager(mViewPager);
     }
 
-    @Override
-    public void update() {
-        //mProgressBar.setVisibility(View.GONE);
-        //startFABAnimation();
-        if (worksRepository.get().isPresent()) {
-            //workAdapter.updateItems(worksRepository.get().get(), true);
-            mBook = worksRepository.get().get();
-        }
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-        worksRepository.addUpdatable(this);
-    }
-
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        worksRepository.removeUpdatable(this);
-    }
+//    @Override
+//    public void update() {
+//        //mProgressBar.setVisibility(View.GONE);
+//        //startFABAnimation();
+//        if (worksRepository.get().isPresent()) {
+//            //workAdapter.updateItems(worksRepository.get().get(), true);
+//            mBook = worksRepository.get().get();
+//        }
+//    }
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        worksRepository.addUpdatable(this);
+//    }
+//
+//
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        worksRepository.removeUpdatable(this);
+//    }
 
     public class SearchObservable extends BaseObservable {
 
@@ -129,6 +144,73 @@ public class BookDetailActivity extends AppCompatActivity implements Updatable {
                 .goTo(ThreadPool.executor)
                 .thenGetFrom(worksSupplier)
                 .compile();
+    }
+
+    private void getRequest() {
+
+        final Request request=new Request.Builder()
+                .get()
+                .tag(this)
+                .url("http://118.178.95.56:8084/api/aono/GetWorkDetail?ID="+workId)
+                .build();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Response response = null;
+                try {
+                    response = client.newCall(request).execute();
+                    if (response.isSuccessful()) {
+//                        Log.i("WY","打印GET响应的数据：" + response.body().string());
+                        JSONObject json = new JSONObject(response.body().string());
+                        Log.e("Hehe",json.toString());
+                        JSONArray jaBooks = json.optJSONArray("data");
+                        Gson gson = new Gson();
+                        List<Work> mBooks = gson.fromJson(jaBooks.toString(), new TypeToken<List<Work>>() {
+                        }.getType());
+                        mBook = mBooks.get(0);
+
+                        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+                        collapsingToolbar.setTitle(mBook.getName());
+                        //collapsingToolbar.setExpandedTitleColor(Color.BLUE);
+
+//                        ImageView ivImage = (ImageView) findViewById(R.id.ivImage);
+//                        Glide.with(ivImage.getContext())
+//                                .load("http://118.178.95.56:8086/UploadFiles/"+mBook.getImageUrlDetail())
+//                                .fitCenter()
+//                                .into(ivImage);
+
+//                        Glide.with(getApplicationContext())
+//                                .load("http://118.178.95.56:8086/UploadFiles/"+mBook.getImageUrlDetail())
+//                                .asBitmap()
+//                                .into(new BitmapImageViewTarget(ivImage) {
+//                                    @Override
+//                                    protected void setResource(Bitmap resource) {
+//                                        //Play with bitmap
+//                                        super.setResource(resource);
+//                                    }
+//                                });
+
+                        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+                        setupViewPager(mViewPager);
+
+                        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+                        tabLayout.addTab(tabLayout.newTab().setText("速览"));
+                        tabLayout.addTab(tabLayout.newTab().setText("简介"));
+                        tabLayout.addTab(tabLayout.newTab().setText("花絮"));
+                        tabLayout.setupWithViewPager(mViewPager);
+
+                    } else {
+                        throw new IOException("Unexpected code " + response);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 
 
