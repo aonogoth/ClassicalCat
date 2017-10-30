d0bc2bfda057eef76c172475aae69b03b691ee16
aono <aonogoth@gmail.com>
2017-10-30 21:45:00 +0800

    qi

--- app/src/main/java/com/classical/aono/classicalcat/activity/MainActivity.java
+++ app/src/main/java/com/classical/aono/classicalcat/activity/MainActivity.java
@@ -1,16 +1,14 @@
 package com.classical.aono.classicalcat.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.classical.aono.classicalcat.R;
 import com.classical.aono.classicalcat.fragment.AboutFragment;
@@ -18,8 +16,6 @@ import com.classical.aono.classicalcat.fragment.BooksFragment;
 import com.classical.aono.classicalcat.fragment.CategoryGdmzFragment;
 import com.classical.aono.classicalcat.fragment.CategoryJdmzFragment;
 import com.classical.aono.classicalcat.fragment.CategoryZtmzFragment;
-import com.classical.aono.classicalcat.fragment.SettingFragment;
-import com.classical.aono.classicalcat.fragment.SettingSCFragment;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -89,9 +85,8 @@ public class MainActivity extends AppCompatActivity
         mToolbar.setTitle("关于");
     }
     private void switchToSetting() {
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new SettingSCFragment()).commit();
-        getFragmentManager().beginTransaction().replace(R.id.frame_content, new SettingFragment()).commit();
-        mToolbar.setTitle("设置");
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 
 
--- app/src/main/java/com/classical/aono/classicalcat/activity/SettingsActivity.java
+++ app/src/main/java/com/classical/aono/classicalcat/activity/SettingsActivity.java
@@ -1,13 +1,98 @@
 package com.classical.aono.classicalcat.activity;
 
+import android.app.Activity;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.classical.aono.classicalcat.R;
+import com.classical.aono.classicalcat.fragment.SettingFragment;
 
 /**
  * Created by gotha on 2017/10/29.
  */
 
-public class SettingsActivity  {
+public class SettingsActivity  extends AppCompatPreferenceActivity {
+
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object o) {
+            String stringValue = o.toString();
+
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in the preference's 'entries' list.
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+
+                // Set the summary to reflect the new value.
+                preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
+
+            } else if (preference instanceof RingtonePreference) {
+                // For ringtone preferences, look up the correct display value using RingtoneManager.
+                if (TextUtils.isEmpty(stringValue)) {
+                    // Empty values correspond to 'silent' (no ringtone).
+                    preference.setSummary("标准");
+
+                } else {
+                    Ringtone ringtone = RingtoneManager.getRingtone(preference.getContext(), Uri.parse(stringValue));
+
+                    if (ringtone == null) {
+                        // Clear the summary if there was a lookup error.
+                        preference.setSummary(null);
+                    } else {
+                        // Set the summary to reflect the new ringtone display name.
+                        String name = ringtone.getTitle(preference.getContext());
+                        preference.setSummary(name);
+                    }
+                }
+
+            } else {
+                // For all other preferences, set the summary to the value's simple string representation.
+                preference.setSummary(stringValue);
+            }
+            return false;
+        }
+    };
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+        // Trigger the listener immediately with the preference's current value.
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), ""));
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("设置");
+        }
+        addPreferencesFromResource(R.xml.preferences_settings);
+        //getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingFragment()).commit();
+
+        bindPreferenceSummaryToValue(findPreference("zihaoshezhi"));
+        bindPreferenceSummaryToValue(findPreference("hangjianjushezhi"));
+
+    }
+
+
 
 }
